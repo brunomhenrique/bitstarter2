a326c1e2b04292153f12d791eda899de7a4f3c36
Bruno Henrique <brunomhenrique@yahoo.com.br>
2013-07-11 15:45:13 +0000

    homework3

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+#!/usr/bin/env node
+var fs = require('fs');
 var express = require('express');
-
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(16);
+
+var buffer = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
